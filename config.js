3c6d5c5969ea261748ae53bafb10d6522e8ae63b
filip842000 <filip842000@gmail.com>
2025-05-02 12:42:36 +0200

    General config

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
 
 	// General
 	name: 'Filip',
-	imageBackground: false,
+	imageBackground: true,
 	openInNewTab: true,
 	twelveHourFormat: false,
 
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '4225a8aa66642d637e64b6f0de8913a2', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '44.687226',
+	defaultLongitude: '10.027630',
 
 	// Autochange
 	autoChangeTheme: true,
